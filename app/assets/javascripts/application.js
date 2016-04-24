17bfa28efc5affec0703ce9a37f3861ce79a35af
Kev <keam.corp@gmail.com>
2016-04-23 21:26:10 -0500

    animation images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
+//= require bootstrap
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require_tree .
